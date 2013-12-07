eb8afc91bb1d5eb8096c53d9bed910e623905696
liudanyun <liudanyun@gmail.com>
2013-12-07 23:05:21 +0800

    add photo view controller

---
+++ DYHybrid/DYGeometryUtil.h
@@ -0,0 +1,14 @@
+//
+//  DYGeometryUtil.h
+//  DYHybrid
+//
+//  Created by danyun on 12/7/13.
+//  Copyright (c) 2013 liudanyun@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface DYGeometryUtil : NSObject
+- (CGRect)CGRectResetY:(CGFloat)y;
+@end
